dc9280c66b7c073465a76eee55bf1f29de9dc9ba
Victor Pino <victopin0@gmail.com>
2017-05-01 23:31:25 -0400

    feat(intranet): First Commit init project

---
+++ intranet/static/bower_components/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+$('.carousel.carousel-slider').carousel({full_width: true});
